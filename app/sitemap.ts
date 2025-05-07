8f00b6f8132608ed506c5eb314ed4b6c3179d81b
956MB <bays@956mb.com>
2025-05-07 00:05:55 -0500

    update sitemap and robots hopefully

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,31 @@
+import { designGroups } from "../lib/designs";
+import { neographyGroups } from "../lib/scripts";
+
 export default async function sitemap() {
-    const routes = ["", "/projects", "/designs", "/neography", "/extras"].map(
-        (route) => ({
-            url: `https://956mb.com${route}`,
+    const baseRoutes = [
+        "",
+        "/projects",
+        "/designs",
+        "/neography",
+        "/extras",
+    ].map((route) => ({
+        url: `https://956mb.com${route}`,
+        lastModified: new Date().toISOString().split("T")[0],
+    }));
+
+    const designSlugs = designGroups.flatMap((group) =>
+        group.items.map((entry) => ({
+            url: `https://956mb.com/designs/${entry.id}`,
             lastModified: new Date().toISOString().split("T")[0],
-        }),
+        })),
     );
 
-    return [...routes];
+    const scriptSlugs = neographyGroups.flatMap((group) =>
+        group.items.map((entry) => ({
+            url: `https://956mb.com/neography/${entry.id}`,
+            lastModified: new Date().toISOString().split("T")[0],
+        })),
+    );
+
+    return [...baseRoutes, ...designSlugs, ...scriptSlugs];
 }
