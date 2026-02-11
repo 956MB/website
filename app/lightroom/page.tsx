8f338bc2f4358234c8c6ef9afb9e592bc88e9693
956MB <bays@956mb.com>
2026-02-10 19:20:18 -0600

    lightroom prerender

--- app/lightroom/page.tsx
+++ app/lightroom/page.tsx
@@ -3,6 +3,7 @@ import { baseUrl, name, og } from "lib/info";
 import { pageMetadata } from "lib/interfaces";
 import { lightroom_section } from "lib/lightroom";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = pageMetadata(
     baseUrl,
@@ -15,6 +16,12 @@ export default function PhotographyPage() {
     const photos = lightroom_section.items;
 
     return (
-        <LightroomGrid photos={photos} />
+        <Suspense
+            fallback={
+                <div className="mx-6 flex w-full max-w-screen-lg justify-center sm:mx-7" />
+            }
+        >
+            <LightroomGrid photos={photos} />
+        </Suspense>
     );
 }
